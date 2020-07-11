3f4e8898e929f3934c5da2f88ed616963370f76b
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-07-11 17:50:02 +0530

    Initial commit

---
+++ app/src/main/java/com/ratnasrivastava/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,60 @@
+package com.ratnasrivastava.downloadingwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (MalformedURLException e) {
+                e.getStackTrace();
+                return "Malformed URL";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "IOException";
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        String result = null;
+        try {
+            result = downloadTask.execute("https://www.zappycode.com").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.d("unique7", "result= " + result);
+    }
+}
